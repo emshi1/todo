0c45fe216e56535577ef2afa71445709dc572ae1
emshi1 <56219594+emshi1@users.noreply.github.com>
2020-11-04 23:08:50 +0700

    Add files via upload

---
+++ precache-manifest.11355b3641b7ef01523c0fb021b06dca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdcd52c64f873b8ec3786f3627996da7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bf100fcd36e7c115c00",
+    "url": "/static/css/main.fba8d7e0.chunk.css"
+  },
+  {
+    "revision": "dfbd4930fd65f662ab2e",
+    "url": "/static/js/2.5e90b6db.chunk.js"
+  },
+  {
+    "revision": "7c8c498c154c5c7fc14cf0ad148613b4",
+    "url": "/static/js/2.5e90b6db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf100fcd36e7c115c00",
+    "url": "/static/js/main.1ad5ba41.chunk.js"
+  },
+  {
+    "revision": "5d12d8f82b275bac6ce4",
+    "url": "/static/js/runtime-main.db44dbce.js"
+  }
+]);
